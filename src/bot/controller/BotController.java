be1c9da7eb27b853e1af9df9f37f7b9a2fcc64fa
tristangaebler <tgae6529@740s-pl-115.local>
2015-12-16 09:19:29 -0700

    Created data members and the constructor

--- src/bot/controller/BotController.java
+++ src/bot/controller/BotController.java
@@ -7,6 +7,17 @@ import bot.model.EV3Bot;
 
 public class BotController
 {
+	private String message;
+	private int xPosition, yPosition;
+	private long waitTime;
+	
+	public BotController() 
+	{		
+		this.xPosition = 50;
+		this.yPosition = 100;
+		
+	}
+	
 	public void start() 
 	{
 		
