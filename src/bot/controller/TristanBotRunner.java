b81691b2ed0fe4e34b526902f792d6cc7f9310c2
tristangaebler <tgae6529@740s-pl-115.local>
2015-12-16 08:47:47 -0700

    Created the Robot Lejos project

---
+++ src/bot/controller/TristanBotRunner.java
@@ -0,0 +1,13 @@
+package bot.controller;
+
+public class TristanBotRunner
+{
+
+	public static void main(String[] args)
+	{
+		BotController myBotController = new BotController();
+		myBotController.start();
+
+	}
+
+}
